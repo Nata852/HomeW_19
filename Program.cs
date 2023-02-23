95ea235642e7bd2f7e5421cabe6cd15925fe91bf
Natali <natlyly3@mail.ru>
2023-02-23 12:02:37 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿int GetNumber(string message)
+{
+    int result = 0;
+    while(true)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Повторите ввод ");
+        }
+    }
+    return result;
+}
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+int len =number.Length;
+
+if (len == 5)
+{
+if (number[0] == number[4] && number[1] == number[3])
+{
+    Console.WriteLine($"{number} - Палиндром");
+}
+else
+{
+Console.WriteLine($"{number} - Не палиндром");
+}
+}
